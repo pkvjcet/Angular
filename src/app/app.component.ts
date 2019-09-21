e8108bfdeef41776615a07e37337537d193e0de5
pkvjcet <princekurian2000@gmail.com>
2019-09-21 16:06:25 +0530

    upto add course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name="Prince";
-  course="Fullstack development";
-  buttonactive=false;
-  constructor(){
-    setTimeout(() => {
-      this.buttonactive=true;
-    }, 10000);
-  }
-  changeCourse(){
-    this.course="MEAN Stack Development";
-  }
-
+  
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,8 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  name="Prince";
+  course="Fullstack development";
+  buttonactive=false;
+  constructor(){
+    setTimeout(() => {
+      this.buttonactive=true;
+    }, 10000);
+  }
+  changeCourse(){
+    this.course="MEAN Stack Development";
+  }
 
-  constructor() { }
+
+  
 
   ngOnInit() {
   }
