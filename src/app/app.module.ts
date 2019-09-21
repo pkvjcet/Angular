7c547900315bb75000e7c36ec224de4540b9752e
pkvjcet <princekurian2000@gmail.com>
2019-09-21 15:36:26 +0530

    upto Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AddcourseComponent } from './addcourse/addcourse.component';
+import { ViewcourseComponent } from './viewcourse/viewcourse.component';
+import {Routes,RouterModule} from '@angular/router';
+const appRoutes:Routes=[
+  {path:'',component:HomeComponent},
+  {path:'add',component:AddcourseComponent},
+  {path:'view',component:ViewcourseComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AddcourseComponent,
+    ViewcourseComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent,NavbarComponent]
