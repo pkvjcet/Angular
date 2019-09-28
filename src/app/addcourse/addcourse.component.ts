317e06ba9d23a0f5691cae18a8bd6c18ac3d2627
pkvjcet <princekurian2000@gmail.com>
2019-09-28 10:54:12 +0530

    read values in angular

--- src/app/addcourse/addcourse.component.ts
+++ src/app/addcourse/addcourse.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-addcourse',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addcourse.component.css']
 })
 export class AddcourseComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
   }
+
+  readValues(data:NgForm){
+    console.log(data.value.courseDate);
+  }
+
 
 }
